0098066d051737146c04bfffcbbd61f0831919bb
Jusef N <2770770+jusefn@users.noreply.github.com>
2023-02-24 09:37:58 +0100

    decimal seperator

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,23 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!{"{}", "Usage: \nCommand: timecalc 8:00\nOutput: 8.00"};
+        println!{"{}", "Command: ./timecalc(.exe) 8:00 ,\nOutput: 8,00\n"};
+        println!{"{}", "Command: ./timecalc(.exe) 9.00\nOutput: 9.00"};
+        println!{"{}", "Command: ./timecalc(.exe) 9.00 ,\nOutput: 9,00\n"};
+        println!{"{}", "Command: ./timecalc(.exe) 10,00\nOutput: 10.00"};
+        println!{"{}", "Command: ./timecalc(.exe) 10,00 ,\nOutput: 10,00\n"};
+        return;
+    }
+
     let timeline = &args[1];
+    let mut decimal_separator = "";
 
-
+    if args.len() >= 3 {
+        decimal_separator = &args[2];
+    }
 
     let sans: String;
     if timeline.find(":") != Option::None {
@@ -26,8 +40,16 @@ fn main() {
     minutes += hours*60_f32;
 
     let time_decimal = minutes / 60_f32;
-    let mut time_string: String = format!("{:.2}", time_decimal);
-    time_string = time_string.replace(".", ",");
+    let time_string: String = format!("{:.2}", time_decimal);
 
-    println!("{}", time_string);
+    if decimal_separator == "" {
+        println!("{}", time_string);
+
+    } else {
+        let new_time_string = time_string.replace(".", decimal_separator);
+        println!("{}", new_time_string);
+    }
+
+
+
 }
